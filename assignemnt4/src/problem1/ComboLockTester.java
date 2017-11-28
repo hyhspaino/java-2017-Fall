67a2364274f3e04da23f2b137dafb9142852a233
hyhspaino <565271815@qq.com>
2017-11-27 21:58:39 -0500

    21:58

--- assignemnt4/src/problem1/ComboLockTester.java
+++ assignemnt4/src/problem1/ComboLockTester.java
@@ -3,17 +3,17 @@ package problem1;
 public class ComboLockTester {
 	public static void main(String[] args)
 	{
-		ComboLock lock=new ComboLock(33,33,33);
-		lock.turnRight(66);
-		lock.turnRight(12);
-		lock.turnRight(33);
-		lock.turnLeft(33);
-		lock.turnLeft(66);
-		lock.turnLeft(12);
-		lock.turnLeft(33);
-		lock.turnRight(33);
-		lock.turnRight(33);
-		System.out.println(lock.open());
+		P8_09 lock=new P8_09(33,33,33);
+		lock.turnRight(63);
+		lock.turnLeft(60);
+		lock.turnRight(0);
+//		lock.turnLeft(33);
+//		lock.turnLeft(66);
+//		lock.turnLeft(12);
+//		lock.turnLeft(33);
+//		lock.turnRight(33);
+//		lock.turnRight(33);
+//		System.out.println(lock.open());
 	}
 
 }
