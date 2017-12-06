e15bc3c2380b6aecb449703fe4e414428fccab47
hyhspaino <565271815@qq.com>
2017-12-05 21:41:16 -0500

    1

--- assignemnt4/src/problem3/DecimalSeparatorFormatter.java
+++ assignemnt4/src/problem3/DecimalSeparatorFormatter.java
@@ -8,7 +8,7 @@ public class DecimalSeparatorFormatter implements NumberFormatter {
 		String s = m.toString();
 		char[] arr = s.toCharArray();
 		int len = arr.length;
-		len /= len + len;
+		len %= len + len;
 		char[] array = new char[len];
 		int temp = 0;
 		for (int i = 0; i < array.length; i++) {
@@ -19,6 +19,8 @@ public class DecimalSeparatorFormatter implements NumberFormatter {
 			array[i] = arr[temp];
 			temp++;
 		}
+//		for(int i =0; i<array.length;i++)
+//			System.out.println(array[i]);
 		return array.toString();
 	}
 
--- assignemnt4/src/problem3/FormatTester.java
+++ assignemnt4/src/problem3/FormatTester.java
@@ -11,26 +11,27 @@ public class FormatTester {
 		String pathname = "./src/Tester.txt";
 		File filename = new File(pathname); 
 		InputStreamReader reader = new InputStreamReader(new FileInputStream(filename)); 
+		@SuppressWarnings("resource")
 		BufferedReader br = new BufferedReader(reader); 
 		String line = "";
 		line = br.readLine();
-		while (line != null) {
-			line = br.readLine(); 
-		}
+//		while (line != null) {
+//			line = br.readLine(); 
+//		}
 		String SplitBy = " ";
 		String[] temp = line.split(SplitBy);
 		Integer[] number = new Integer[temp.length];
-		for (int i = 0; i < number.length; i++)
+		for (int i = 0; i < number.length; i++) 
 			number[i] = Integer.parseInt(temp[i]);
 		AccountingFormatter a1= new AccountingFormatter();
 		for (int i = 0; i < number.length; i++)
 			System.out.println(a1.format(number[i]));
-		BaseFormatter a2= new BaseFormatter(8);
-		for (int i = 0; i < number.length; i++)
-			System.out.println(a2.format(number[i]));
-		BaseFormatter a3= new BaseFormatter(2);
-		for (int i = 0; i < number.length; i++)
-			System.out.println(a3.format(number[i]));
+//		BaseFormatter a2= new BaseFormatter(8);
+//		for (int i = 0; i < number.length; i++)
+//			System.out.println(a2.format(number[i]));
+//		BaseFormatter a3= new BaseFormatter(2);
+//		for (int i = 0; i < number.length; i++)
+//			System.out.println(a3.format(number[i]));
 		DecimalSeparatorFormatter a4= new DecimalSeparatorFormatter();
 		for (int i = 0; i < number.length; i++)
 			System.out.println(a4.format(number[i]));
